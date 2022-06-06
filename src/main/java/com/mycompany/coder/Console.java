7ed300fd245dcb63fb2c699095c1307947a7fb80
Matheus Fernando <matheus.fernando.coder@gmail.com>
2022-06-06 13:10:19 -0300

    commit sobre console

--- src/main/java/com/mycompany/coder/Console.java
+++ src/main/java/com/mycompany/coder/Console.java
@@ -1,5 +1,27 @@
 package com.mycompany.coder;
 
+import java.util.Scanner;
+
 public class Console {
     
+    public static void main(String[] args) {
+        
+        System.out.printf("Salário: %.1f%n", 1234.5678);
+        System.out.printf("Nome: %s%n", "João");
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite o seu nome: ");
+        String nome = entrada.nextLine();
+        
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = entrada.nextLine();
+        
+        System.out.println("Seu nome completo é: " + nome + " " + sobrenome);
+        
+        System.out.println("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+        
+        System.out.println("Você tem " + idade + " anos de idade.");       
+    }
 }
